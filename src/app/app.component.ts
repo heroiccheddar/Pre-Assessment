c96fb263ba6084094bc041104fdfbe62f73c317e
Aaron Hampson <aaron.hampson1@t-mobile.com>
2020-10-13 14:56:12 -0400

    Add functionality to skeleton app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pre-assessment';
+  value = 0;
+
+  increment(): void {
+    this.value++;
+  }
+
+  decrement(): void {
+    this.value--;
+  }
+
+  evenOrOdd(value): string {
+    return value % 2 === 0 ? 'even' : 'odd';
+  }
+
+  isPrimeNumber(value): string {
+    if (value === 1)
+    {
+      return ' not ';
+    }
+    else if (value === 2)
+    {
+      return '';
+    }else
+    {
+      for (let i = 2; i < value; i++)
+      {
+        if (value % i === 0)
+        {
+          return ' not ';
+        }
+      }
+      return '';
+    }
+  }
+
+  squareRoot(value): number {
+    return Math.sqrt(value);
+  }
+
+  reset(): void {
+    this.value = 0;
+  }
+
+  random(): void {
+    this.value = Math.floor(Math.random() * 1000) + 1;
+  }
+
+  mystery(): void {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FlexModule} from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
